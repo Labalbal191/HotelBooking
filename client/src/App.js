d58c5d8918ac0968d57e2ee775690d7e41248c90
Labalbal191 <filipnaklicki@gmail.com>
2022-04-23 18:32:02 +0200

    Implemented rooms data transfer from backend to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import './App.css';
 import Navbar from "./components/Navbar";
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+import Homescreen from './screens/Homescreen';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <BrowserRouter>
+      
+      <Route path ="/home" exact component = {Homescreen} />
+
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function Homescreen() {
+    
+    const[rooms, setrooms] = useState([])
+   
+    useEffect(() => {   
+        try {
+            async function gettingRooms(){     
+                const data = (await axios.get('./api/rooms/getallrooms')).data
+                setrooms(data)
+              }
+              gettingRooms()
+        }
+        catch (error) {
+            console.log(error)
+        }   
+    },[])
+
   return (
-    <div>Home screen</div>
+    <div> 
+        <h1>Home</h1>
+        <h1>pokoje:{rooms.length}</h1>
+    </div>
   )
 }
 
