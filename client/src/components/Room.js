beba45c1608ddf2b06af575ec28d7fa39eb77fd3
Labalbal191 <filipnaklicki@gmail.com>
2022-05-01 19:33:53 +0200

    Room carousel implemented

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Modal, Button, Carousel } from 'react-bootstrap'
 
 function Room({ room }) {
-  console.log(JSON.stringify(room))
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="row bx_shadow">
       <div className='col-md-4'>
@@ -15,10 +19,28 @@ function Room({ room }) {
           <p>Type: {room.type}</p>
         </b>
         <div style={{ float: 'right' }}>
-          <button className='btn btn-primary'> See the details</button>
+          <button className='details_btn btn-primary' onClick={handleShow}> See the details</button>
         </div>
       </div>
 
+      <Modal show={show} onHide={handleClose} size='lg'>
+        <Modal.Header closeButton>
+          <Modal.Title>{room.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Carousel>
+            {room.imageurls.map(url => {
+              return <Carousel.Item>
+                <img
+                  className="d-block fullsizeimg"
+                  src={url}
+                />
+              </Carousel.Item>
+            })}
+          </Carousel>
+          <p>{room.description}</p>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
