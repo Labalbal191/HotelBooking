cb4d5fd3489c5827678c5e6fe4f322584a691659
Labalbal191 <filipnaklicki@gmail.com>
2022-05-01 18:54:47 +0200

    Styling changes applied

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,20 +1,24 @@
 import React from 'react'
 
-function Room({room}) {
-  console.log(JSON.stringify(room)) 
+function Room({ room }) {
+  console.log(JSON.stringify(room))
   return (
-    <div className="row">
+    <div className="row bx_shadow">
       <div className='col-md-4'>
-          <img src={room.imageurls[0]} alt={room.imageurls[0]} className="smallimg"/>
+        <img src={room.imageurls[0]} alt={room.imageurls[0]} className="smallimg" />
       </div>
       <div className='col-md-7'>
         <h1>{room.name}</h1>
-        <p>Max people: {room.maxcount}</p>
-        <p>Phonenumber: {room.phonenumber}</p>
-        <p>Type: {room.type}</p>
-
+        <b>
+          <p>Max people: {room.maxcount}</p>
+          <p>Phonenumber: {room.phonenumber}</p>
+          <p>Type: {room.type}</p>
+        </b>
+        <div style={{ float: 'right' }}>
+          <button className='btn btn-primary'> See the details</button>
+        </div>
       </div>
-        
+
     </div>
   )
 }
