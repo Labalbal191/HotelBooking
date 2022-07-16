81834a778f1b1aa503803f57bdf8d81ecaa0cabd
Labalbal191 <filipnaklicki@gmail.com>
2022-07-16 18:58:05 +0200

    filters in progress

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -40,16 +40,16 @@ function Homescreen() {
         settodate(moment(dates[1]).format('DD-MM-YYYY'))
 
         var temprooms = []
-             
+
         for (const room of duplicaterooms) {
             var available = false
             if (room.currentbookings.length > 0) {
                 for (var booking of room.currentbookings) {
                     if (
-                        !moment(moment(dates[0]).format('DD-MM-YYYY')).isBetween(booking.fromdate, booking.todate) 
-                            &&
+                        !moment(moment(dates[0]).format('DD-MM-YYYY')).isBetween(booking.fromdate, booking.todate)
+                        &&
                         !moment(moment(dates[1]).format('DD-MM-YYYY')).isBetween(booking.fromdate, booking.todate)
-                    ){
+                    ) {
                         if (
                             moment(dates[0]).format('DD-MM-YYYY') !== booking.fromdate &&
                             moment(dates[0]).format('DD-MM-YYYY') !== booking.todate &&
@@ -61,22 +61,47 @@ function Homescreen() {
                     }
                 }
             }
-            if(available == true || room.currentbookings.length ==0){
+            if (available == true || room.currentbookings.length == 0) {
                 temprooms.push(room)
             }
             setrooms(temprooms)
         }
     }
 
+    function filterByPeople(e){
+        const temp = duplicaterooms.filter(room=>room)
+    }
+
     return (
 
         <div className='container'>
-            <div className='row mt-5'>
-                <div className='col-md-4'>
+            <div className='row mt-5 col-md-11 bs'>
+                <div className='col-md-5'>
                     <RangePicker format='DD-MM-YYYY' onChange={filterByDate} />
                 </div>
+                
+                <div className='col-md-3'>
+                    <select className='form-control'>
+                        <option value="Wszystkie"> Wszystkie pokoje</option>
+                        <option value="50"> 0-50 zł</option>
+                        <option value="100"> 100-150 zł</option>
+                        <option value="150"> 150-200 zł</option>
+                        <option value="200"> 200+ zł</option>
+                    </select>
+                </div>
+                <div className='col-md-3'>
+                    <select className='form-control'>
+                        <option value="Wszystkie"> Wszystkie pokoje</option>
+                        <option value="1"> 1 osobowe</option>
+                        <option value="2"> 2 osobowe</option>
+                        <option value="3"> 3 osobowe</option>
+                        <option value="4"> 4+ osobowe</option>
+                    </select>
+                </div>
+
+
             </div>
-            <div className='row justify-content-center mt-5 '>
+            <div className='row justify-content-center mt-3'>
                 {loading ?
                     (<Loader />)
                     : rooms.length > 1 ?
